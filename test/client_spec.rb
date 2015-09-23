ce7544a7f38ca7b66e07798d9b8ccb3dbad84c71
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-09-22 17:16:21 -0700

    trim VCR fixtures and tests

--- test/client_spec.rb
+++ test/client_spec.rb
@@ -57,8 +57,8 @@ describe AlsoEnergy::Client do
         end
 
         fixture = YAML.load_file("test/fixtures/also_energy_invalid_login.yml")
-        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["http_interactions"][1]["request"]["body"]["string"]
-          ).to_return(:body => fixture["http_interactions"][1]["response"]["body"]["string"])
+        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["request"]["body"]["string"]
+          ).to_return(:body => fixture["response"]["body"]["string"])
 
         assert_raises(AlsoEnergy::AuthError) do
           @client.login
@@ -74,40 +74,14 @@ describe AlsoEnergy::Client do
         end
 
         fixture = YAML.load_file("test/fixtures/also_energy_successful_login.yml")
-        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["http_interactions"][1]["request"]["body"]["string"]
-          ).to_return(:body => fixture["http_interactions"][1]["response"]["body"]["string"])
+        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["request"]["body"]["string"]
+          ).to_return(:body => fixture["response"]["body"]["string"])
 
         @client.login.must_equal true
 
       end
 
     end
-
-    # describe "#login" do
-    #   it "raises an exception for login failure" do
-    #     @client = AlsoEnergy::Client.new do |c|
-    #       c.username = "SpaceDoge"
-    #       c.password = "12345"
-    #       c.session_id = "totallynotnil"
-    #     end
-    #     VCR.use_cassette "also_energy_invalid_login" do
-    #       assert_raises(AlsoEnergy::AuthError) do
-    #         @client.login
-    #       end
-    #     end
-    #   end
-    #
-    #   it "returns true for a successful authentication" do
-    #     @client = AlsoEnergy::Client.new do |c|
-    #       c.username = "SpaceDoge"
-    #       c.password = "12345"
-    #       c.session_id = "totallynotnil"
-    #     end
-    #     VCR.use_cassette "also_energy_successful_login" do
-    #       @client.login.must_equal true
-    #     end
-    #   end
-    # end
 
   end
 
@@ -127,8 +101,8 @@ describe AlsoEnergy::Client do
         end
 
         fixture = YAML.load_file("test/fixtures/also_energy_get_sites_query_success.yml")
-        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["http_interactions"][1]["request"]["body"]["string"]
-          ).to_return(:body => fixture["http_interactions"][1]["response"]["body"]["string"])
+        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["request"]["body"]["string"]
+          ).to_return(:body => fixture["response"]["body"]["string"])
 
         @client.get_sites.wont_be_nil
 
@@ -142,8 +116,8 @@ describe AlsoEnergy::Client do
         end
 
         fixture = YAML.load_file("test/fixtures/also_energy_get_sites_query_failure.yml")
-        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["http_interactions"][1]["request"]["body"]["string"]
-          ).to_return(:body => fixture["http_interactions"][1]["response"]["body"]["string"])
+        stub_request(:post, "https://www.alsoenergy.com/WebAPI/WebAPI.svc").with(:body => fixture["request"]["body"]["string"]
+          ).to_return(:body => fixture["response"]["body"]["string"])
 
         assert_raises(AlsoEnergy::QueryError) do
           @client.get_sites
@@ -152,33 +126,6 @@ describe AlsoEnergy::Client do
       end
 
     end
-
-    # describe "#get_sites" do
-    #   it "successfully returns an array of AlsoEnergy::Site objects from the API" do
-        # @client = AlsoEnergy::Client.new do |c|
-        #   c.username = "SpaceDoge"
-        #   c.password = "12345"
-        #   c.session_id = "totallynotnil"
-        # end
-    #     VCR.use_cassette "also_energy_get_sites_query_success" do
-    #       @client.get_sites
-    #       @client.site_objects.wont_be_empty
-    #     end
-    #   end
-    #
-    #   it "raises an exception when the query fails" do
-    #     @client = AlsoEnergy::Client.new do |c|
-    #       c.username = "SpaceDoge"
-    #       c.password = "12345"
-    #       c.session_id = "totallynotnil"
-    #     end
-    #     VCR.use_cassette "also_energy_get_sites_query_failure" do
-    #       assert_raises(AlsoEnergy::QueryError) do
-    #         @client.get_sites
-    #       end
-    #     end
-    #   end
-    # end
 
   end
 
